8916717c1d914de050854cacbce28f070d62dbb7
r3dcrosse <mrdavidwayman@gmail.com>
2016-02-13 03:05:39 -0800

    Added time since tweet was posted

--- TweetCell.swift
+++ TweetCell.swift
@@ -15,6 +15,7 @@ class TweetCell: UITableViewCell {
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var usernameLabel: UILabel!
     @IBOutlet weak var tweetLabel: UILabel!
+    @IBOutlet weak var timeAgoLabel: UILabel!
     
     @IBOutlet weak var retweetedLabel: UILabel!
     @IBOutlet weak var retweetedIcon: UIImageView!
@@ -29,8 +30,38 @@ class TweetCell: UITableViewCell {
             nameLabel.text = tweet.user!.name
             usernameLabel.text = "@\(tweet.user!.screenname!)"
             thumbImageView.setImageWithURL(tweet.user!.profileImageUrl!)
+            timeAgoLabel.text = calculateTimeStamp(tweet.createdAt!.timeIntervalSinceNow)
             
         }
+    }
+    
+    func calculateTimeStamp(timeTweetPostedAgo: NSTimeInterval) -> String {
+        // Turn timeTweetPostedAgo into seconds, minutes, hours, days, or years
+        var rawTime = Int(timeTweetPostedAgo)
+        var timeAgo: Int = 0
+        var timeChar = ""
+        
+        rawTime = rawTime * (-1)
+        
+        // Figure out time ago
+        if (rawTime <= 60) { // SECONDS
+            timeAgo = rawTime
+            timeChar = "s"
+        } else if ((rawTime/60) <= 60) { // MINUTES
+            timeAgo = rawTime/60
+            timeChar = "m"
+        } else if (rawTime/60/60 <= 24) { // HOURS
+            timeAgo = rawTime/60/60
+            timeChar = "h"
+        } else if (rawTime/60/60/24 <= 365) { // DAYS
+            timeAgo = rawTime/60/60/24
+            timeChar = "d"
+        } else if (rawTime/(3153600) <= 1) { // YEARS
+            timeAgo = rawTime/60/60/24/365
+            timeChar = "y"
+        }
+        
+        return "\(timeAgo)\(timeChar)"
     }
     
     
