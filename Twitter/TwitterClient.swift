74f6f755f19c49a08b58d8a7d98a5383002af579
r3dcrosse <mrdavidwayman@gmail.com>
2016-02-25 15:03:14 -0800

    Added abstract function comments

--- Twitter/TwitterClient.swift
+++ Twitter/TwitterClient.swift
@@ -25,6 +25,7 @@ class TwitterClient: BDBOAuth1SessionManager {
         return Static.instance
     }
     
+    // Added abstract function for getting statuses with certain endpoints from twitter API
     func getEndpointTimelineWithParams(endpoint: String!, params: NSDictionary?, completion: (tweets: [Tweet]?, error: NSError?) -> ()) {
         TwitterClient.sharedInstance.GET(
             "1.1/statuses/\(endpoint).json",
@@ -40,23 +41,6 @@ class TwitterClient: BDBOAuth1SessionManager {
                 completion(tweets: nil, error: error)
         })
     }
-    
-//    func homeTimelineWithParams(params: NSDictionary?, completion: (tweets: [Tweet]?, error: NSError?) -> ()) {
-//        TwitterClient.sharedInstance.GET(
-//            "1.1/statuses/home_timeline.json",
-//            parameters: params,
-//            success: { (operation: NSURLSessionDataTask!, response: AnyObject?) -> Void in
-//                print("home timeline: \(response!)")
-//                let tweets = Tweet.tweetsWithArray(response as! [NSDictionary])
-//                
-//                completion(tweets: tweets, error: nil)
-//            },
-//            failure: { (operation: NSURLSessionDataTask?, error: NSError!) -> Void in
-//                print("error getting home timeline")
-//                completion(tweets: nil, error: error)
-//        })
-//
-//    }
     
     func loginWithCompletion(completion: (user: User?, error: NSError?) -> ()) {
         loginCompletion = completion
